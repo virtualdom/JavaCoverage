9e02b10b5c8ca4ddb532d5307695b1bffa366384
Dominic Joseph <dtpjoseph@gmail.com>
2017-03-10 20:30:40 -0600

    Add OTF instrumentation with ASM and write coverage information to file.

--- src/cs6367/transformer/ClassTransformVisitor.java
+++ src/cs6367/transformer/ClassTransformVisitor.java
@@ -5,16 +5,24 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 class ClassTransformVisitor extends ClassVisitor implements Opcodes {
-
-    public ClassTransformVisitor(final ClassVisitor cv) {
+    String className;
+    String outputFilepath;
+    public ClassTransformVisitor(final ClassVisitor cv, String outputFilepath) {
         super(ASM5, cv);
+        this.outputFilepath = outputFilepath;
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+      className = name;
+      super.visit(version, access, name, signature, superName, interfaces);
     }
 
     @Override
     public MethodVisitor visitMethod(final int access, final String name,
             final String desc, final String signature, final String[] exceptions) {
         MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
-        return mv == null ? null : new MethodTransformVisitor(mv, name);
+        return mv == null ? null : new MethodTransformVisitor(mv, name, className, outputFilepath);
     }
 }
 
--- src/cs6367/transformer/TransformClassFile.java
+++ src/cs6367/transformer/TransformClassFile.java
@@ -5,28 +5,29 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
-
-// import java.io.FileInputStream;
-// import java.io.FileOutputStream;
-
-// import org.objectweb.asm.ClassReader;
-// import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
 
 public class TransformClassFile implements ClassFileTransformer {
+  String outputFilepath;
+  public TransformClassFile (String outputFilepath) {
+    this.outputFilepath = outputFilepath;
+  }
+
 	public byte[] transform(ClassLoader loader, String className,
 			Class classBeingRedefined, ProtectionDomain protectionDomain,
 			byte[] classfileBuffer) throws IllegalClassFormatException {
 		byte[] byteCode = classfileBuffer;
 
-		// OUR CODE MUST GO IN HERE.
-		// IT MUST TRANSFORM byteCode DEPENDING ON
-		// HOW WE WANT TO GATHER COVERAGE INFORMATION.
-		// USE ASM TO DO THIS, NOT JAVASSIST
+    if ("cs6367/test/TestInstrumentation".equals(className)) {
+        ClassReader reader = new ClassReader(byteCode);
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        ClassTransformVisitor visitor = new ClassTransformVisitor(writer, outputFilepath);
+        reader.accept(visitor, 0);
+        return writer.toByteArray();
+    }
 
-		return byteCode;
+    return null;
 	}
 
 	// // this is the professor's original code
